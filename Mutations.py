3466900cb58b5dc00d7e28995d22dde48f648b21
Faahim Sadman <57841708+SadManFahIm@users.noreply.github.com>
2021-08-23 14:01:31 +0600

    Created

---
+++ Mutations.py
@@ -0,0 +1,12 @@
+def mutate_string(string, position, character):
+    # Mutations in Python - HackerRank Solution START
+    pos = position+1
+    Output = string[:position]+character+string[pos:]
+    return Output
+    # Mutations in Python - HackerRank Solution END
+
+if __name__ == '__main__':
+    s = input()
+    i, c = input().split()
+    s_new = mutate_string(s, int(i), c)
+    print(s_new)
